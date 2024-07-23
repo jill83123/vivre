11b8c44228b7c599dbb17f2bc55a68bcfc9edc8e
YU-FANG <jill83123@gmail.com>
2024-07-24 01:24:31 +0800

    feat: 完成登入與註冊頁

--- main.js
+++ main.js
@@ -68,3 +68,27 @@ window.addEventListener('scroll', () => {
   navbarContent.classList.add('py-lg-9');
   navbarContent.classList.remove('py-lg-4');
 });
+
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(() => {
+  'use strict';
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  const forms = document.querySelectorAll('.needs-validation');
+
+  // Loop over them and prevent submission
+  Array.from(forms).forEach((form) => {
+    form.addEventListener(
+      'submit',
+      (event) => {
+        if (!form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+
+        form.classList.add('was-validated');
+      },
+      false
+    );
+  });
+})();
